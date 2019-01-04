096dcae68ec44c3a23b2b317f68d161db4bfc468
康小建 <1243228706@qq.com>
2019-01-04 18:12:03 +0800

    增加宽高获取

--- index.js
+++
@@ -1,9 +0,0 @@
-
-exports.Base = require('./lib/Base/production.min.js');
-exports.ImageUpload = require('./lib/Base/production.min.js');
-
-// if (process.env.NODE_ENV === 'production') {
-//     module.exports = require('./lib/production.min.js');
-// } else {
-//     module.exports = require('./lib/development.js');
-// }
